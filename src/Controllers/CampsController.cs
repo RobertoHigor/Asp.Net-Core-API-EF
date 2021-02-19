430c1511fa63529b829a1a12e6d9b91beb9568c1
Roberto Higor <roberto.higor@outlook.com>
2021-02-19 14:18:09 -0300

    Adicionado rota Create com validações

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System;
 using CoreCodeCamp.Models;
 using AutoMapper;
+using Microsoft.AspNetCore.Routing;
 
 namespace CoreCodeCamp.Controllers
 {
@@ -14,11 +15,13 @@ namespace CoreCodeCamp.Controllers
     {
         private readonly ICampRepository _repository;
         private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
 
-        public CampsController(ICampRepository repository, IMapper mapper)
+        public CampsController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
         {
             _repository = repository;
             _mapper = mapper;
+            _linkGenerator = linkGenerator;
         }
 
         // Ao utilizar ActionResult<Tipo>, é dado como Ok
@@ -26,7 +29,7 @@ namespace CoreCodeCamp.Controllers
         // É possível utilizar IActionResult caso não queira explicitar
         [HttpGet]
         public async Task<ActionResult<CampModel[]>> Get(bool includeTalks = false)
-        {            
+        {
             try
             {
                 // Por ser async, é necessario await
@@ -39,7 +42,7 @@ namespace CoreCodeCamp.Controllers
             catch (Exception)
             {
                 // Não existe um método para retoranr 500 Internal Server ERror
-                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");                
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
             }
         }
 
@@ -59,5 +62,53 @@ namespace CoreCodeCamp.Controllers
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
             }
         }
+
+        public async Task<ActionResult<CampModel>> Post(CampModel model)
+        {
+            try
+            {
+                // Validar no controller campo único para que não se repita
+                var campExist = await _repository.GetCampAsync(model.Moniker);
+                if (campExist != null)
+                {
+                    return BadRequest("Moniker is in use");
+                }
+
+                // Gerar o Link antes de criar o objeto, caso haja algum problema
+                var location = _linkGenerator.GetPathByAction("GetCampById",
+                    "Camps",
+                    new { moniker = model.Moniker });
+
+                /*
+                 * if (ModelState.IsValid)
+                 * Serve para validar a Model, usando as annotations.
+                 * Somente é utilizado caso queira algo customizado ou não esteja usando [ApiController]
+                 */
+
+                if (string.IsNullOrWhiteSpace(location))
+                {
+                    return BadRequest("Could not use current moniker");
+                }
+
+                var camp = _mapper.Map<Camp>(model);
+                _repository.Add(camp);
+                if (await _repository.SaveChangesAsync())
+                {
+                    // uri para pegar o novo objeto
+                    // Mapear do jeito $"/api/camps/{camp.Moniker}" funciona, porém a string está hardcoded
+                    // Para isso existe a biblioteca do Asp.NET Core LinkGenerator
+                    return Created("", _mapper.Map<CampModel>(camp));
+                }
+                
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+
+            return BadRequest();
+        }
+
+            
     }
 }
