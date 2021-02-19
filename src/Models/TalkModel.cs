ef65126deb7564935f373681314b72222d926983
Roberto Higor <roberto.higor@outlook.com>
2021-02-19 19:18:35 -0300

    Validação do talks

--- src/Models/TalkModel.cs
+++ src/Models/TalkModel.cs
@@ -1,12 +1,17 @@
-﻿namespace CoreCodeCamp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreCodeCamp.Models
 {
     public class TalkModel
     {
         // Id é necessário para identificar o Talk
         public int TalkId { get; set; }
-
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
+        [StringLength(4000, MinimumLength = 20)]
         public string Abstract { get; set; }
+        [Range(100, 500)]
         public int Level { get; set; }
 
         public SpeakerModel Speaker { get; set; }
